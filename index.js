dc7fb5ac353aa3b1aa4ca8b76ec07e208af2be07
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-15 11:38:36 -0300

    fix reference to mp3

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var IPod = require('ipod');
-var audio = './birl.mp3';
+var audio = __dirname + '/birl.mp3';
 var ipod = new IPod(audio);
 
-ipod.on('playing', function() {
-	ipod.stop();
-});
-
-module.exports = function() {
+function birl() {
 	ipod.play();
-};
+	ipod.on('playing', function() {
+		ipod.stop();
+	});
+}
+
+module.exports = birl;
